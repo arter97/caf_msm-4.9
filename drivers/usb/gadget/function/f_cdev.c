34bf932c33fbf338d996917575a4c4ff13cb3787
Phanendra Yarra <c_pyarra@qti.qualcomm.com>
2018-04-04 02:50:32 -0700

    Revert "Revert "Revert "usb: gadget: Add hacks for f_cdev to use SMD"""
    This reverts commit 624bb6a74de3ddc912e80209e475110e6dfc539a.
    
    Change-Id: Ia5b83228eafd167f3006deb29191273fb20ba7a7
    Signed-off-by: c_pyarra <pyarra@codeaurora.org>

--- drivers/usb/gadget/function/f_cdev.c
+++ drivers/usb/gadget/function/f_cdev.c
@@ -46,8 +46,6 @@
 #include <asm/ioctls.h>
 #include <asm-generic/termios.h>
 
-#include "u_serial.h"
-
 #define DEVICE_NAME "at_usb"
 #define MODULE_NAME "msm_usb_bridge"
 #define NUM_INSTANCE 3
@@ -62,6 +60,36 @@
 
 #define GS_LOG2_NOTIFY_INTERVAL		5  /* 1 << 5 == 32 msec */
 #define GS_NOTIFY_MAXPACKET		10 /* notification + 2 bytes */
+
+struct cserial {
+	struct usb_function		func;
+	struct usb_ep			*in;
+	struct usb_ep			*out;
+	struct usb_ep			*notify;
+	struct usb_request		*notify_req;
+	struct usb_cdc_line_coding	port_line_coding;
+	u8				pending;
+	u8				q_again;
+	u8				data_id;
+	u16				serial_state;
+	u16				port_handshake_bits;
+	/* control signal callbacks*/
+	unsigned int (*get_dtr)(struct cserial *p);
+	unsigned int (*get_rts)(struct cserial *p);
+
+	/* notification callbacks */
+	void (*connect)(struct cserial *p);
+	void (*disconnect)(struct cserial *p);
+	int (*send_break)(struct cserial *p, int duration);
+	unsigned int (*send_carrier_detect)(struct cserial *p,
+						unsigned int val);
+	unsigned int (*send_ring_indicator)(struct cserial *p,
+						unsigned int val);
+	int (*send_modem_ctrl_bits)(struct cserial *p, int ctrl_bits);
+
+	/* notification changes to modem */
+	void (*notify_modem)(void *port, int ctrl_bits);
+};
 
 struct f_cdev {
 	struct cdev		fcdev_cdev;
@@ -123,8 +151,8 @@ static DEFINE_MUTEX(chardev_ida_lock);
 static int usb_cser_alloc_chardev_region(void);
 static void usb_cser_chardev_deinit(void);
 static void usb_cser_read_complete(struct usb_ep *ep, struct usb_request *req);
-//static int usb_cser_connect(struct f_cdev *port);
-//static void usb_cser_disconnect(struct f_cdev *port);
+static int usb_cser_connect(struct f_cdev *port);
+static void usb_cser_disconnect(struct f_cdev *port);
 static struct f_cdev *f_cdev_alloc(char *func_name, int portno);
 static void usb_cser_free_req(struct usb_ep *ep, struct usb_request *req);
 
@@ -321,7 +349,6 @@ static inline struct f_cdev *cser_to_port(struct cserial *cser)
 	return container_of(cser, struct f_cdev, port_usb);
 }
 
-/*
 static unsigned int convert_acm_sigs_to_uart(unsigned int acm_sig)
 {
 	unsigned int uart_sig = 0;
@@ -335,7 +362,6 @@ static unsigned int convert_acm_sigs_to_uart(unsigned int acm_sig)
 
 	return uart_sig;
 }
-*/
 
 static void port_complete_set_line_coding(struct usb_ep *ep,
 		struct usb_request *req)
@@ -407,8 +433,7 @@ usb_cser_setup(struct usb_function *f, const struct usb_ctrlrequest *ctrl)
 			w_value & ACM_CTRL_DTR ? 1 : 0,
 			w_value & ACM_CTRL_RTS ? 1 : 0);
 		if (port->port_usb.notify_modem)
-			port->port_usb.notify_modem(&port->port_usb, 0,
-								w_value);
+			port->port_usb.notify_modem(port, w_value);
 
 		break;
 
@@ -470,7 +495,7 @@ static int usb_cser_set_alt(struct usb_function *f, unsigned int intf,
 	if (port->port_usb.in->driver_data) {
 		dev_dbg(&cdev->gadget->dev,
 			"reset port(%s)\n", port->name);
-		gsmd_disconnect(&port->port_usb, 0);
+		usb_cser_disconnect(port);
 	}
 	if (!port->port_usb.in->desc || !port->port_usb.out->desc) {
 		dev_dbg(&cdev->gadget->dev,
@@ -484,10 +509,7 @@ static int usb_cser_set_alt(struct usb_function *f, unsigned int intf,
 		}
 	}
 
-	gsmd_connect(&port->port_usb, 0);
-	port->is_connected = true;
-	port->port_usb.pending = false;
-	port->port_usb.q_again = false;
+	usb_cser_connect(port);
 	return rc;
 }
 
@@ -499,8 +521,7 @@ static void usb_cser_disable(struct usb_function *f)
 	dev_dbg(&cdev->gadget->dev,
 		"port(%s) deactivated\n", port->name);
 
-	gsmd_disconnect(&port->port_usb, 0);
-	port->is_connected = false;
+	usb_cser_disconnect(port);
 	usb_ep_disable(port->port_usb.notify);
 	port->port_usb.notify->driver_data = NULL;
 }
@@ -676,7 +697,6 @@ static void usb_cser_free_req(struct usb_ep *ep, struct usb_request *req)
 	}
 }
 
-/*
 static void usb_cser_free_requests(struct usb_ep *ep, struct list_head *head)
 {
 	struct usb_request	*req;
@@ -687,7 +707,6 @@ static void usb_cser_free_requests(struct usb_ep *ep, struct list_head *head)
 		usb_cser_free_req(ep, req);
 	}
 }
-*/
 
 static struct usb_request *
 usb_cser_alloc_req(struct usb_ep *ep, unsigned int len, size_t extra_sz,
@@ -724,12 +743,6 @@ static int usb_cser_bind(struct usb_configuration *c, struct usb_function *f)
 		if (status < 0)
 			return status;
 		cser_string_defs[0].id = status;
-	}
-
-	status = gsmd_setup(cdev->gadget, 1);
-	if (status) {
-		pr_err("gsmd_setup failed\n");
-		return status;
 	}
 
 	status = usb_interface_id(c, f);
@@ -833,7 +846,6 @@ static void usb_cser_unbind(struct usb_configuration *c, struct usb_function *f)
 	usb_cser_free_req(port->port_usb.notify, port->port_usb.notify_req);
 }
 
-/*
 static int usb_cser_alloc_requests(struct usb_ep *ep, struct list_head *head,
 		int num, int size, size_t extra_sz,
 		void (*cb)(struct usb_ep *ep, struct usb_request *))
@@ -856,7 +868,6 @@ static int usb_cser_alloc_requests(struct usb_ep *ep, struct list_head *head,
 
 	return 0;
 }
-*/
 
 static void usb_cser_start_rx(struct f_cdev *port)
 {
@@ -928,7 +939,6 @@ static void usb_cser_read_complete(struct usb_ep *ep, struct usb_request *req)
 	wake_up(&port->read_wq);
 }
 
-/*
 static void usb_cser_write_complete(struct usb_ep *ep, struct usb_request *req)
 {
 	unsigned long flags;
@@ -950,13 +960,13 @@ static void usb_cser_write_complete(struct usb_ep *ep, struct usb_request *req)
 	switch (req->status) {
 	default:
 		pr_debug("unexpected %s status %d\n", ep->name, req->status);
-		FALL THROUGH
+		/* FALL THROUGH */
 	case 0:
-		normal completion
+		/* normal completion */
 		break;
 
 	case -ESHUTDOWN:
-		disconnect
+		/* disconnect */
 		pr_debug("%s shutdown\n", ep->name);
 		break;
 	}
@@ -1018,7 +1028,7 @@ static void usb_cser_stop_io(struct f_cdev *port)
 	in = port->port_usb.in;
 	out = port->port_usb.out;
 
-	disable endpoints, aborting down any active I/O
+	/* disable endpoints, aborting down any active I/O */
 	usb_ep_disable(out);
 	out->driver_data = NULL;
 	usb_ep_disable(in);
@@ -1037,7 +1047,6 @@ static void usb_cser_stop_io(struct f_cdev *port)
 	usb_cser_free_requests(in, &port->write_pool);
 	spin_unlock_irqrestore(&port->port_lock, flags);
 }
-*/
 
 int f_cdev_open(struct inode *inode, struct file *file)
 {
@@ -1409,7 +1418,6 @@ static long f_cdev_ioctl(struct file *fp, unsigned int cmd,
 	return ret;
 }
 
-/*
 static void usb_cser_notify_modem(void *fport, int ctrl_bits)
 {
 	int temp;
@@ -1483,7 +1491,7 @@ void usb_cser_disconnect(struct f_cdev *port)
 
 	usb_cser_stop_io(port);
 
-	lower DTR to modem
+	/* lower DTR to modem */
 	usb_cser_notify_modem(port, 0);
 
 	spin_lock_irqsave(&port->port_lock, flags);
@@ -1492,7 +1500,6 @@ void usb_cser_disconnect(struct f_cdev *port)
 	port->nbytes_to_port_bridge = 0;
 	spin_unlock_irqrestore(&port->port_lock, flags);
 }
-*/
 
 static const struct file_operations f_cdev_fops = {
 	.owner = THIS_MODULE,
--- drivers/usb/gadget/function/u_smd.c
+++ drivers/usb/gadget/function/u_smd.c
@@ -22,7 +22,6 @@
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
 #include <linux/device.h>
-#include <linux/platform_device.h>
 #include <linux/delay.h>
 #include <linux/slab.h>
 #include <linux/termios.h>
@@ -39,7 +38,7 @@
 
 static struct workqueue_struct *gsmd_wq;
 
-#define SMD_N_PORTS	1
+#define SMD_N_PORTS	2
 #define CH_OPENED	0
 #define CH_READY	1
 struct smd_port_info {
@@ -51,6 +50,9 @@ struct smd_port_info {
 struct smd_port_info smd_pi[SMD_N_PORTS] = {
 	{
 		.name = "DS",
+	},
+	{
+		.name = "UNUSED",
 	},
 };
 
@@ -66,7 +68,7 @@ struct gsmd_port {
 	struct list_head	write_pool;
 	struct work_struct	pull;
 
-	struct cserial		*port_usb;
+	struct gserial		*port_usb;
 
 	struct smd_port_info	*pi;
 	struct delayed_work	connect_work;
@@ -84,7 +86,7 @@ struct gsmd_port {
 #define SMD_ACM_CTRL_DCD		0x01
 #define SMD_ACM_CTRL_DSR		0x02
 #define SMD_ACM_CTRL_BRK		0x04
-#define SMD_ACM_CTRL_RI			0x08
+#define SMD_ACM_CTRL_RI		0x08
 	unsigned int		cbits_to_laptop;
 
 	/* pkt counters */
@@ -631,7 +633,7 @@ static void gsmd_notify_modem(void *gptr, u8 portno, int ctrl_bits)
 {
 	struct gsmd_port *port;
 	int temp;
-	struct cserial *gser = gptr;
+	struct gserial *gser = gptr;
 
 	if (portno >= n_smd_ports) {
 		pr_err("%s: invalid portno#%d\n", __func__, portno);
@@ -683,7 +685,7 @@ static void gsmd_notify_modem(void *gptr, u8 portno, int ctrl_bits)
 			~port->cbits_to_modem);
 }
 
-int gsmd_connect(struct cserial *gser, u8 portno)
+int gsmd_connect(struct gserial *gser, u8 portno)
 {
 	unsigned long flags;
 	int ret;
@@ -735,7 +737,7 @@ int gsmd_connect(struct cserial *gser, u8 portno)
 	return 0;
 }
 
-void gsmd_disconnect(struct cserial *gser, u8 portno)
+void gsmd_disconnect(struct gserial *gser, u8 portno)
 {
 	unsigned long flags;
 	struct gsmd_port *port;
@@ -1000,7 +1002,7 @@ int gsmd_setup(struct usb_gadget *g, unsigned int count)
 				__func__);
 		return -ENOMEM;
 	}
-	extra_sz = 0;
+	extra_sz = g->extra_buf_alloc;
 
 	for (i = 0; i < count; i++) {
 		mutex_init(&smd_ports[i].lock);
