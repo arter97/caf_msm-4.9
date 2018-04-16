d1a555e78845657fc61480f34eeb6db483f12bda
Ram Prakash Gupta <rampraka@codeaurora.org>
2018-04-16 11:17:22 +0530

    mmc: host: sdhci-msm: Toggle FIFO write clk after MCLK ungated
    During GCC level clock gating of MCLK, the async FIFO
    gets into some hang condition, such that for the next
    transfer after MCLK ungating, first bit of CMD response
    doesn't get written in to the FIFO. This cause the CPSM
    to hang eventually leading to SW timeout.
    
    To fix the issue, toggle the FIFO write clock after
    MCLK ungated to get the FIFO pointers and flags to
    valid states.
    
    Change-Id: I29faefbcb814fcc36fb8afcf7ec2cb51c553b5e6
    Signed-off-by: Ram Prakash Gupta <rampraka@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -173,6 +173,8 @@
 #define MAX_DRV_TYPES_SUPPORTED_HS200	4
 #define MSM_AUTOSUSPEND_DELAY_MS 100
 
+#define RCLK_TOGGLE 0x2
+
 struct sdhci_msm_offset {
 	u32 CORE_MCI_DATA_CNT;
 	u32 CORE_MCI_STATUS;
@@ -3456,18 +3458,32 @@ static void sdhci_msm_set_clock(struct sdhci_host *host, unsigned int clock)
 					| CORE_HC_SELECT_IN_EN), host->ioaddr +
 					msm_host_offset->CORE_VENDOR_SPEC);
 		}
+		/*
+		 * After MCLK ugating, toggle the FIFO write clock to get
+		 * the FIFO pointers and flags to valid state.
+		 */
 		if (msm_host->tuning_done ||
 				(card && mmc_card_strobe(card) &&
-				 msm_host->enhanced_strobe)) {
-			/* Write 1 to SDCC_HC_REG_DLL_CONFIG_3 register with DLL_CONFIG_3[1] */
-			writel_relaxed(((readl_relaxed(host->ioaddr + 0x258)) | 0x2),
-				host->ioaddr + 0x258);
-			mb();
+				msm_host->enhanced_strobe)) {
+			/*
+			 * set HC_REG_DLL_CONFIG_3[1] to select MCLK as
+			 * DLL input clock
+			 */
+			writel_relaxed(((readl_relaxed(host->ioaddr +
+				msm_host_offset->CORE_DDR_CONFIG))
+				| RCLK_TOGGLE), host->ioaddr +
+				msm_host_offset->CORE_DDR_CONFIG);
+			/* ensure above write as toggling same bit quickly */
+			wmb();
 			udelay(2);
-			/* Write 0 to SDCC_HC_REG_DLL_CONFIG_3 register with DLL_CONFIG_3[1] */
-			writel_relaxed(((readl_relaxed(host->ioaddr + 0x258)) & ~0x2),
-				host->ioaddr + 0x258);
-			mb();
+			/*
+			 * clear HC_REG_DLL_CONFIG_3[1] to select RCLK as
+			 * DLL input clock
+			 */
+			writel_relaxed(((readl_relaxed(host->ioaddr +
+				msm_host_offset->CORE_DDR_CONFIG))
+				& ~RCLK_TOGGLE), host->ioaddr +
+				msm_host_offset->CORE_DDR_CONFIG);
 		}
 		if (!host->mmc->ios.old_rate && !msm_host->use_cdclp533) {
 			/*
