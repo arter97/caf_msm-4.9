feb3d1c92aad9e23caa9cd89bf2ac42849466a42
Peter Zijlstra <peterz@infradead.org>
2018-04-17 16:33:14 +0200

    mm: VMA sequence count
    Wrap the VMA modifications (vma_adjust/unmap_page_range) with sequence
    counts such that we can easily test if a VMA is changed.
    
    The unmap_page_range() one allows us to make assumptions about
    page-tables; when we find the seqcount hasn't changed we can assume
    page-tables are still valid.
    
    The flip side is that we cannot distinguish between a vma_adjust() and
    the unmap_page_range() -- where with the former we could have
    re-checked the vma bounds against the address.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    
    [Port to 4.12 kernel]
    [Build depends on CONFIG_SPECULATIVE_PAGE_FAULT]
    [Introduce vm_write_* inline function depending on
     CONFIG_SPECULATIVE_PAGE_FAULT]
    [Fix lock dependency between mapping->i_mmap_rwsem and vma->vm_sequence by
     using vm_raw_write* functions]
    [Fix a lock dependency warning in mmap_region() when entering the error
     path]
    [move sequence initialisation INIT_VMA()]
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
    Change-Id: Ibc23ef3b9dbb80323c0f24cb06da34b4c3a8fa71
    Patch-mainline: linux-mm @ 17 Apr 2018 16:33:14
    [vinmenon@codeaurora.org: trivial merge conflict fixes]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- mm/memory.c
+++ mm/memory.c
@@ -1291,6 +1291,7 @@ void unmap_page_range(struct mmu_gather *tlb,
 	unsigned long next;
 
 	BUG_ON(addr >= end);
+	vm_write_begin(vma);
 	tlb_start_vma(tlb, vma);
 	pgd = pgd_offset(vma->vm_mm, addr);
 	do {
@@ -1300,6 +1301,7 @@ void unmap_page_range(struct mmu_gather *tlb,
 		next = zap_pud_range(tlb, vma, pgd, addr, next, details);
 	} while (pgd++, addr = next, addr != end);
 	tlb_end_vma(tlb, vma);
+	vm_write_end(vma);
 }
 
 
