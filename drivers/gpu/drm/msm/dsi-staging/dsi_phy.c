4ec377ce7bbe99fcc953a55d440fab867046ead3
Aravind Venkateswaran <aravindh@codeaurora.org>
2018-05-14 17:15:17 -0700

    drm/msm/dsi-staging: fix idle power collapse exit sequence for PHY v3
    Ensure that the escape clock is turned on prior to reinitializing
    the DSI PHY when exiting idle power collapse. This will ensure that
    there are no spurious transitions on the lanes after the PHY clamps
    are removed.
    
    Change-Id: Ic7ce1af62581b71d2a610b18381c9c57eb80696c
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    Signed-off-by: Tharun Raj Soma <tsoma@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_phy.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_phy.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -891,6 +891,26 @@ int dsi_phy_disable(struct msm_dsi_phy *phy)
 	mutex_unlock(&phy->phy_lock);
 
 	return rc;
+}
+
+/**
+ * dsi_phy_set_clamp_state() - configure clamps for DSI lanes
+ * @phy:        DSI PHY handle.
+ * @enable:     boolean to specify clamp enable/disable.
+ *
+ * Return: error code.
+ */
+int dsi_phy_set_clamp_state(struct msm_dsi_phy *phy, bool enable)
+{
+	if (!phy)
+		return -EINVAL;
+
+	pr_debug("[%s] enable=%d\n", phy->name, enable);
+
+	if (phy->hw.ops.clamp_ctrl)
+		phy->hw.ops.clamp_ctrl(&phy->hw, enable);
+
+	return 0;
 }
 
 /**
