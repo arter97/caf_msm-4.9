19a0a72ef0548564b7b3a96ea97c074d213ae269
Tharun Kumar Merugu <mtharu@codeaurora.org>
2018-05-17 15:21:08 +0530

    msm: adsprpc: destroy mutex before file free
    Destroy mutex before file free, to avoid use after free of mutex.
    
    Change-Id: I4ff73dc17b15043eacbb299219a379bfd1a8efa6
    Acked-by: Himateja Reddy <hmreddy@qti.qualcomm.com>
    Signed-off-by: Tharun Kumar Merugu <mtharu@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -2397,8 +2397,7 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 	spin_unlock(&fl->apps->hlock);
 
 	if (!fl->sctx) {
-		kfree(fl);
-		return 0;
+		goto bail;
 	}
 	spin_lock(&fl->hlock);
 	fl->file_close = 1;
@@ -2417,7 +2416,9 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 		fastrpc_session_free(&fl->apps->channel[cid], fl->sctx);
 	if (fl->secsctx)
 		fastrpc_session_free(&fl->apps->channel[cid], fl->secsctx);
+bail:
 	mutex_destroy(&fl->fl_map_mutex);
+	mutex_destroy(&fl->map_mutex);
 	kfree(fl);
 	return 0;
 }
@@ -2431,7 +2432,6 @@ static int fastrpc_device_release(struct inode *inode, struct file *file)
 			pm_qos_remove_request(&fl->pm_qos_req);
 		if (fl->debugfs_file != NULL)
 			debugfs_remove(fl->debugfs_file);
-		mutex_destroy(&fl->map_mutex);
 		fastrpc_file_free(fl);
 		file->private_data = NULL;
 	}
