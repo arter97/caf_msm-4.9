fe1a6ee608a864a7b51f98b856916e7aae9c8e13
Aravind Venkateswaran <aravindh@codeaurora.org>
2018-07-19 14:53:41 -0700

    drm/msm/dp: fix error checking for memory allocations
    Memory allocation using kzalloc can return a valid pointer
    when a zero size memory is requested. Therefore it is
    important to use IS_ZERO_OR_NULL macro instead of just
    checking for a null pointer prior to any use of the allocated
    memory.
    
    CRs-Fixed: 2279573
    Change-Id: I3a9526347669d77bd5642195ab2681ef96ce580a
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -1053,7 +1053,7 @@ static ssize_t debugfs_misr_read(struct file *file,
 		return 0;
 
 	buf = kzalloc(max_len, GFP_KERNEL);
-	if (!buf)
+	if (ZERO_OR_NULL_PTR(buf))
 		return -ENOMEM;
 
 	mutex_lock(&display->display_lock);
@@ -1171,7 +1171,7 @@ static ssize_t debugfs_alter_esd_check_mode(struct file *file,
 		return 0;
 
 	buf = kzalloc(len, GFP_KERNEL);
-	if (!buf)
+	if (ZERO_OR_NULL_PTR(buf))
 		return -ENOMEM;
 
 	if (copy_from_user(buf, user_buf, user_len)) {
@@ -1243,7 +1243,7 @@ static ssize_t debugfs_read_esd_check_mode(struct file *file,
 	}
 
 	buf = kzalloc(len, GFP_KERNEL);
-	if (!buf)
+	if (ZERO_OR_NULL_PTR(buf))
 		return -ENOMEM;
 
 	esd_config = &display->panel->esd_config;
