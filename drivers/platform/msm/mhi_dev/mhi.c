9b372e5620f03c73e78cd7fa2b92d2134e6ba2a2
Siddartha Mohanadoss <smohanad@codeaurora.org>
2019-10-09 10:55:32 -0700

    msm: mhi_dev: Fix invalid client access
    Check if handle passed from clients is valid before dereference.
    
    Change-Id: I68a1a826f7b85a7e24f2cb75916c50e6788536e9
    Signed-off-by: Siddartha Mohanadoss <smohanad@codeaurora.org>

--- drivers/platform/msm/mhi_dev/mhi.c
+++ drivers/platform/msm/mhi_dev/mhi.c
@@ -1974,6 +1974,7 @@ free_ereqs:
 	ch->ereqs = NULL;
 free_client:
 	kfree(*handle_client);
+	*handle_client = NULL;
 exit:
 	mutex_unlock(&ch->ch_lock);
 	return rc;
@@ -1984,6 +1985,11 @@ int mhi_dev_channel_isempty(struct mhi_dev_client *handle)
 {
 	struct mhi_dev_channel *ch;
 	int rc;
+
+	if (!handle) {
+		mhi_log(MHI_MSG_ERROR, "Invalid channel access\n");
+		return -EINVAL;
+	}
 
 	ch = handle->channel;
 	if (!ch)
@@ -1999,6 +2005,11 @@ int mhi_dev_close_channel(struct mhi_dev_client *handle)
 {
 	struct mhi_dev_channel *ch;
 	int rc = 0;
+
+	if (!handle) {
+		mhi_log(MHI_MSG_ERROR, "Invalid channel access\n");
+		return -EINVAL;
+	}
 
 	ch = handle->channel;
 
